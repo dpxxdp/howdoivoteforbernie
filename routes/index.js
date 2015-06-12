f654f1001c1248e7ad26c126d3a3f7fc728cf480
Daniel Barrett <dan.pk.bar@gmail.com>
2015-06-12 02:03:11 -0400

    cleanup routes/index

--- routes/index.js
+++ routes/index.js
@@ -6,21 +6,6 @@ var express = require('express');
 var router = express.Router();
 var vote = require('../public/data/vote.js');
 
-function readJsonFileSync(filepath, encoding){
-
-    if (typeof (encoding) === 'undefined'){
-        encoding = 'utf8';
-    }
-    var file = fs.readFileSync(filepath, encoding);
-    return JSON.parse(file);
-}
-
-function getConfig(file){
-
-    var filepath = express.static(__dirname + file);
-    return readJsonFileSync(filepath);
-}
-
 /* GET home page. */
 router.get('/', function(req, res) {
 	res.json(vote);
