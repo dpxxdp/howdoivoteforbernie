1a24cefcfdf0aa81251510077eacfcf21d245f60
Daniel Barrett <dan.pk.bar@gmail.com>
2015-06-17 00:26:06 -0400

    angular up and running

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -7,6 +7,7 @@ function mainController($scope, $http) {
 	$scope.voteInfo = { 'states' : [], };
 	$scope.selectedState = {};  //TODO Browser cached selectedState maybe?
 	$scope.stateCache = {};
+	$scope.currentInput = '';
 	$scope.error = '';
 
 	$http.get('/api/v0.1')
@@ -29,29 +30,58 @@ function mainController($scope, $http) {
 			});
 	};
 
-	$scope.selectState = function(abbrev) {
-		//console.log("a:" + JSON.stringify($scope.selectedState));
-		//if(abbrev in $scope.stateCache) {
-		//	console.log("b:" + JSON.stringify($scope.selectedState));
-		//	$scope.selectedState = $scope.stateCache[abbrev];
-		//}
-		//else {
+	$scope.selectStateByAbbrev = function(abbrev) {
 
 		var success = false;
 
 		for (var i = 0; i < $scope.voteInfo.states.length; i++) {
-			if($scope.voteInfo.states[i].abbreviation === abbrev) {
+			if($scope.voteInfo.states[i].abbreviation.toUpperCase() === abbrev.toUpperCase()) {
 				$scope.selectedState = $scope.voteInfo.states[i];
-				$scope.stateCache[abbrev] = $scope.voteInfo.states[i];
+				//$scope.stateCache[abbrev] = $scope.voteInfo.states[i];
 				$scope.error = '';
 				success = true;
 			}
 		}
 		if(success !== true) {
 			console.log('Error: Cannot select state: ' + abbrev);
-			$scope.error = abbrev + ' is not a state!';
+			$scope.error = abbrev + ' is not a US state!';
 		}
+	};
 
-		//}
+	$scope.selectStateByName = function(name) {
+
+		var success = false;
+
+		for (var i = 0; i < $scope.voteInfo.states.length; i++) {
+			if($scope.voteInfo.states[i].name.toUpperCase() === name.toUpperCase()) {
+				$scope.selectedState = $scope.voteInfo.states[i];
+				//$scope.stateCache[name] = $scope.voteInfo.states[i];
+				$scope.error = '';
+				success = true;
+			}
+		}
+		if(success !== true) {
+			console.log('Error: Cannot select state: ' + name);
+			$scope.error = name + ' is not a US state!';
+		}
+	};
+
+
+	$scope.getSelectedState = function() {
+		return ($scope.selectedState && $scope.selectedState.name);
+	};
+
+	$scope.areWeMissingInformation = function() {
+		//console.log($scope.selectedState);
+		return !$scope.selectedState.info;
+	};
+
+	$scope.selectStateFromCurrentInput = function() {
+		if($scope.currentInput.length===2) {
+			$scope.selectStateByAbbrev($scope.currentInput);
+		}
+		else {
+			$scope.selectStateByName($scope.currentInput);
+		}
 	};
 }
